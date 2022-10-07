8ad138df9d4e19aca3843c662155e025e94db032
Luca Bassi <luca@argoware.com>
2022-10-07 14:12:40 +0200

    Stop the dockers before the database dump

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
-su postgres -c "cd /postgres; pg_dumpall > db.out"
 docker-compose down
+su postgres -c "cd /postgres; pg_dumpall > db.out"
 for dir in gitlab mattermost jenkins taiga sonarqube postgres
 do
 	rsync -avzh /$dir backup:/backup/$dir
