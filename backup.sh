1ead9bbc91254b65bec6618c1d2119fb534c3313
Luca <luca.tagliavini5@studio.unibo.it>
2022-10-07 17:11:04 +0200

    make backup work

--- backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-docker-compose down
-su postgres -c "cd /postgres; pg_dumpall > db.out"
-for dir in gitlab mattermost jenkins taiga sonarqube postgres
-do
-	rsync -avzh /$dir backup:/backup/$dir
-done
-docker-compose up -d
-rm /postgres/db.out
