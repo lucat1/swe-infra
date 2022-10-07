3698defb11fe93ad189a5412c5689e1dc8507cac
Luca Bassi <luca@argoware.com>
2022-10-07 14:08:18 +0200

    Add postgres backup

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
-#!/bin/bash -ex
-
-for dir in gitlab mattermost jenkins taiga sonarqube
+#!/bin/bash -x
+su postgres -c "cd /postgres; pg_dumpall > db.out"
+docker-compose down
+for dir in gitlab mattermost jenkins taiga sonarqube postgres
 do
 	rsync -avzh /$dir backup:/backup/$dir
 done
+docker-compose up -d
+rm /postgres/db.out
